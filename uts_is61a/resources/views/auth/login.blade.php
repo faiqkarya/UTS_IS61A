63c2175c75d16f8baf79627181ab405a340e6c02
M Faiq Hanzhalah <mfaiqhanzhalah@gmail.com>
2024-05-17 21:57:35 +0700

    Update login.blade.php

--- uts_is61a/resources/views/auth/login.blade.php
+++ uts_is61a/resources/views/auth/login.blade.php
@@ -69,24 +69,24 @@
                                             <div class="input-group-text">
                                               <span class="fas fa-lock"></span>
                                             </div>
-                                          </div>
+                                    </div>
                                         </div>
                                         <div class="row">
-                                          <div class="col-8">
-                                            <div class="icheck-primary">
-                                              <input type="checkbox" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                                              <label for="remember">
-                                                Remember Me
-                                              </label>
+                                            <div class="col-8">
+                                                <div class="icheck-primary">
+                                                    <input type="checkbox" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                    <label for="remember">
+                                                    Remember Me
+                                                    </label>
+                                                </div>
                                             </div>
-                                          </div>
-                                          <!-- /.col -->
-                                          <div class="col-4">
-                                            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                                          </div>
-                                          <!-- /.col -->
+                                            <!-- /.col -->
+                                            <div class="col-4">
+                                                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                                            </div>
+                                            <!-- /.col -->
                                         </div>
-                                      </form>
+                                    </form>
                                     <hr>
                                     <div class="text-center">
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
