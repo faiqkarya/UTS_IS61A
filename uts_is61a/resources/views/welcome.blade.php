3ab310d71fe6266604228300bdee47addef6e22c
M Faiq Hanzhalah <mfaiqhanzhalah@gmail.com>
2024-05-17 21:27:04 +0700

    home

--- uts_is61a/resources/views/welcome.blade.php
+++ uts_is61a/resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="{{asset('fontawesome-free-6.5.2-web/css/all.min.css')}}">
 
         <!-- Styles -->
         <style>
